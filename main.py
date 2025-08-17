b543c947c188f051b39402f6bbf390886f4eee1d
PanKatofel <pankatofel@gmail.com>
2025-08-17 11:35:53 +0200

    Add type speed test to the portfolio

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, url_for, redirect, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from forms import ContactForm
-from env import load_env
+# from env import load_env
 import smtplib
 import os
 
-load_env()
+# load_env()
 # -------------------------------------------------------------------
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ["FLASK_KEY"]
